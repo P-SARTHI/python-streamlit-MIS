93b43b88dd9ce7ed18225de1ea8918174e97525a
Parth Sarthi <152152967+P-SARTHI@users.noreply.github.com>
2024-03-24 14:58:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-# @Email:  contact@pythonandvba.com
-# @Website:  https://pythonandvba.com
-# @YouTube:  https://youtube.com/c/CodingIsFun
-# @Project:  Sales Dashboard w/ Streamlit
+
 
 
 
@@ -9,7 +6,6 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 
 
 # ---- READ EXCEL ----
